2364b9279a1bfbc6efa49ebcfdc691cf12219497
Andrew <xgearh3h3@gmail.com>
2023-07-03 23:55:48 +0100

    random color generator for the website

--- script.js
+++ script.js
@@ -110,3 +110,23 @@ h1.onmouseenter = alert(
   'addEventListening: Great! You are reading the heading!'
 );
 */
+
+// random number generator
+const randomInt = (min, max) =>
+  Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () =>
+  `rgb(${randomInt(0, 255)}, ${randomInt(0, 255)}, ${randomInt(0, 255)})`;
+console.log(randomColor(0, 255));
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  console.log('LINK');
+  this.style.backgroundColor = randomColor;
+});
+
+document.querySelector('.nav__links').addEventListener('click', function (e) {
+  console.log('LINK');
+});
+
+document.querySelector('.nav__link').addEventListener('click', function (e) {
+  console.log('LINK');
+});
